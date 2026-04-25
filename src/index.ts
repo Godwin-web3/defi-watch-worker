49a3f270194b5132e70227c9872ecbbb8f7326db
Godwin-web3 <mbagodwin419@gmail.com>
2026-04-25 09:56:56 +0100

    fix: restore this.run call

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const uniswapInterface = new Interface(uniswapAbi);
 
 export default {
   async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
-    await run(env);
+    await this.run(env);
   },
 
   async run(env: Env) {
