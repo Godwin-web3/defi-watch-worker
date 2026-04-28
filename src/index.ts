5024109f975a72c950b8e3b4e48227ee5a0474ab
Godwin-web3 <mbagodwin419@gmail.com>
2026-04-28 04:14:00 +0100

    feat: wire up uniswap mempool monitor

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import http from 'http';
 import { getTransaction, updateActor, getThreatPrefix, formatActorDescription, calculatePriceImpact, getKV, putKV, supabase, AAVE_ORACLE } from './utils.js';
 import { computeFlowDelta } from './flowTracker.js';
 import { startMempoolMonitor, monitorAaveMempool } from './mempool.js';
+import { startUniswapMempoolMonitor } from './uniswap-mempool.js';
 import { normalizeAlert } from './alerts.js';
 import { Contract, ZeroAddress } from 'ethers';
 
@@ -175,6 +176,7 @@ async function run() {
 
 console.log('Starting DeFi Watch Worker...');
 startMempoolMonitor();
+startUniswapMempoolMonitor();
 const wsProvider = process.env.WS_RPC_URL ? new WebSocketProvider(process.env.WS_RPC_URL) : null;
 if (wsProvider) monitorAaveMempool(wsProvider, process.env.TELEGRAM_BOT_TOKEN!, process.env.TELEGRAM_CHAT_ID!);
 run().catch(console.error);
